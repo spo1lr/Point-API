04573f902adaadd54e8fe50dde7599945e43f54e
spo1lr <choisb7366@gmail.com>
2024-11-02 15:06:51 +0900

    Initial commit

---
+++ src/main/java/com/subin/point/PointApiApplication.java
@@ -0,0 +1,13 @@
+package com.subin.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointApiApplication.class, args);
+    }
+
+}
