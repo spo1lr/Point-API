aefe6bd0b5f1ef56f7c3d3c1d5c1334665814371
spo1lr <choisb7366@gmail.com>
2024-11-03 18:20:47 +0900

    add: PointTransaction (포인트 사용 내역) Entity, Repository

---
+++ src/main/java/com/subin/point/repository/PointTransactionRepository.java
@@ -0,0 +1,12 @@
+package com.subin.point.repository;
+
+import com.subin.point.entity.PointTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PointTransactionRepository extends JpaRepository<PointTransaction, Long> {
+    List<PointTransaction> findByOrderId(String orderId);
+}
