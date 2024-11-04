e31a15d835f974649d46fbf225292164c1a62a1a
spo1lr <choisb7366@gmail.com>
2024-11-04 13:19:08 +0900

    test: 포인트 사용 Test Case

--- src/main/java/com/subin/point/repository/PointTransactionRepository.java
+++ src/main/java/com/subin/point/repository/PointTransactionRepository.java
@@ -1,6 +1,7 @@
 package com.subin.point.repository;
 
 import com.subin.point.entity.PointTransaction;
+import com.subin.point.entity.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,9 @@ import java.util.List;
 
 @Repository
 public interface PointTransactionRepository extends JpaRepository<PointTransaction, Long> {
+    // 주문번호를 이용한 트랜잭션 전체조회
     List<PointTransaction> findByOrderId(String orderId);
+
+    // 이용 타입별 트랜잭션 전체조회
+    List<PointTransaction> findAllByType(TransactionType type);
 }
