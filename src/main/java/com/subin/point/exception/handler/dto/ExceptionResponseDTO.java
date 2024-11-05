47a22458cc8d2b39a9ba0be6256304e78a67bbd1
spo1lr <choisb7366@gmail.com>
2024-11-05 12:56:16 +0900

    add: Exception Handler For BaseEntity

---
+++ src/main/java/com/subin/point/exception/handler/dto/ExceptionResponseDTO.java
@@ -0,0 +1,9 @@
+package com.subin.point.exception.handler.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ExceptionResponseDTO {
+    private String code;
+    private String message;
+}
