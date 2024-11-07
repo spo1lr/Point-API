d92c8c03c8f291fbe2bdca38efe412c9f798aef9
spo1lr <choisb7366@gmail.com>
2024-11-07 15:27:05 +0900

    add: 포인트 사용 취소 Transaction Type

--- src/main/java/com/subin/point/entity/TransactionType.java
+++ src/main/java/com/subin/point/entity/TransactionType.java
@@ -3,10 +3,12 @@ package com.subin.point.entity;
 public enum TransactionType {
     // 포인트 적립
     EARN,
-    // 포인트 사용
-    USE,
     // 포인트 적립 취소
     CANCEL,
+    // 포인트 사용
+    USE,
+    // 포인트 사용 취소
+    USECANCEL,
     // 만료 포인트 재적립
     REISSUE
 }
